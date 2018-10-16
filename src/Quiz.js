9e77fca0d9dad3973f8702cde627b8b272dc90a5
Jay Kudo <jay.kudo@avanade.com>
2018-10-16 04:39:43 -0700

    Updates - Creating a Quiz Component

---
+++ src/Quiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'React'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div />
+        )
+    }
+}
+
+export default Quiz
